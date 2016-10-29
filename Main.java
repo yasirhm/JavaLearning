69986c89ffb758e06f08fde5263a0c2abbb9668d
yasirhm <yasaman.rahimee@gmail.com>
2016-10-29 13:56:42 +0330

    ok

--- Main.java
+++ Main.java
@@ -6,140 +6,109 @@ import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.util.*;
 import java.io.File;
+import javax.swing.text.html.parser.Parser;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.ParserConfigurationException;
+
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
 import org.w3c.dom.Node;
 import org.w3c.dom.Element;
+import org.xml.sax.SAXException;
 
 
 import static java.lang.Integer.parseInt;
+
 import java.math.BigDecimal;
 
 public class Main {
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
 
-        List<Deposit> deposits = new ArrayList<Deposit>() ;
-        XMLParser (deposits);
+        List<Deposit> deposits = new ArrayList<Deposit>();
+        try {
+            parseXML(deposits);
+        } catch (ParserConfigurationException | SAXException | IOException e) {
+            System.exit(1);
+        }
+
         Collections.sort(deposits, new Comparator() {
             @Override
             public int compare(Object depositOne, Object depositTwo) {
                 //use instanceof to verify the references are indeed of the type in question
                 //ascending order
-               // return ((Deposit)depositOne).getPayedInterest()
-                 //       .compareTo(((Deposit)depositTwo).getPayedInterest());
+                // return ((Deposit)depositOne).getPayedInterest()
+                //       .compareTo(((Deposit)depositTwo).getPayedInterest());
                 //descending order
-                 return ((Deposit)depositTwo).getPayedInterest()
-                          .compareTo(((Deposit)depositOne).getPayedInterest());
+                return ((Deposit) depositTwo).getPayedInterest()
+                        .compareTo(((Deposit) depositOne).getPayedInterest());
 
             }
         });
-        doAccess(deposits);
-
         System.out.println("Sorted");
         for (Deposit dp : deposits) {
-            System.out.println(dp.getCustomerNumber() + " - " + dp.getPayedInterest() );
+            System.out.println(dp.getCustomerNumber() + " - " + dp.getPayedInterest());
         }
+        writeIntoAccessFile(deposits);
     }
 
-    private static void XMLParser( List<Deposit> deposits)
-    {
+    private static void parseXML(List<Deposit> deposits) throws ParserConfigurationException, SAXException, IOException {
 
-        try{
-            File inputFile;
-            inputFile = new File("C:/Users/Dotinschool6/Desktop/Dotin School/Project1/src/input.xml");
-            DocumentBuilderFactory dbFactory
-                    = DocumentBuilderFactory.newInstance();
+        File inputFile;
+        inputFile = new File("src/input1.xml");
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
 
-            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-            Document doc = dBuilder.parse(inputFile);
+        DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+        Document doc = dBuilder.parse(inputFile);
+        doc.getDocumentElement().normalize();
+        NodeList nList = doc.getElementsByTagName("deposit");
 
-            doc.getDocumentElement().normalize();
+        for (int i = 0; i < nList.getLength(); i++) {
+            Node nNode = nList.item(i);
+            if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+                Element eElement = (Element) nNode;
+                try {
+                    Class depositTypeClass = Class.forName(eElement.getElementsByTagName("depositType").item(0).getTextContent());
+                    DepositType depositType = (DepositType) depositTypeClass.newInstance();
 
-            System.out.println("Root element :"
-                    + doc.getDocumentElement().getNodeName());
-            NodeList nList = doc.getElementsByTagName("deposit");
-            System.out.println(nList.getLength());
-
-            for(int i=0; i<nList.getLength();i++){
-                Node nNode = nList.item(i);
-                if (nNode.getNodeType() == Node.ELEMENT_NODE) { ////??
-                    Element eElement = (Element) nNode;
-
-                    /*try {
-                        Class depositType = Class.forName(eElement.getElementsByTagName("depositType").item(0).getTextContent());
-                        DepositType dType = (DepositType) depositType.newInstance();
-                    }catch (ClassNotFoundException err){
-                        System.out.println("Exception!");
-                        err.println(
-                                "Reflection error trying to invoke " + className + ".");
-                    }*/
-
-                    try {
-                        Class depositType = Class.forName(eElement.getElementsByTagName("depositType").item(0).getTextContent());
-                        DepositType dType = (DepositType) depositType.newInstance();
-
-                        Class deposit = Class.forName("Deposit");
-                        Deposit de = (Deposit) deposit.getConstructor(new Class[]{String.class, Integer.class, BigDecimal.class, DepositType.class}).newInstance(eElement.getElementsByTagName("customerNumber").item(0).getTextContent(),
-                                parseInt(eElement.getElementsByTagName("durationInDays").item(0).getTextContent()),
-                                new BigDecimal(eElement.getElementsByTagName("depositBalance").item(0).getTextContent().replaceAll(",", "")),
-                                dType
-                        );
-                        de.calculatePayedInterest();
-                        deposits.add(de);
-                    }catch(Exception exp){
-                        exp.printStackTrace();
-                    }
-
-
+                    Deposit deposit = new Deposit(eElement.getElementsByTagName("customerNumber").item(0).getTextContent(),
+                            parseInt(eElement.getElementsByTagName("durationInDays").item(0).getTextContent()),
+                            new BigDecimal(eElement.getElementsByTagName("depositBalance").item(0).getTextContent().replaceAll(",", "")),
+                            depositType
+                    );
+                    deposit.calculatePayedInterest();
+                    deposits.add(deposit);
+                } catch (DurationDaysException e) {
+                    System.out.println("Error: " + e.getMessage());
+                } catch (BalanceException e) {
+                    System.out.println("Error: " + e.getMessage());
+                } catch (ClassNotFoundException e) {
+                    System.out.println("Error: " + e.getMessage() + " is unknown deposit type!");
+                } catch (ReflectiveOperationException e) {
+                    System.out.println("Error: " + e.getMessage());
                 }
-
             }
-
-        }catch(Exception e){
-            System.out.println("Exception In parsing XML File!");
-            e.printStackTrace();
         }
     }
 
-
-    private static void doAccess( List<Deposit> deposits)
-    {
-
+    private static void writeIntoAccessFile(List<Deposit> deposits) {
         try {
-
             File file = new File("Project1RandomAccessFile.out");
             RandomAccessFile raf = new RandomAccessFile(file, "rw");
-
             // Read a character
             //byte ch = raf.readByte();
             //System.out.println("Read first character of file: " + (char)ch);
-
-            // Now read the remaining portion of the line.
-            // This will print out from where the file pointer is located
-            // (just after the '+' character) and print all remaining characters
-            // up until the end of line character.
             //System.out.println("Read full line: " + raf.readLine());
-
             // Seek to the end of file
             raf.seek(file.length());
-
-            // Append to the end of the file
-            raf.write(0x0A);
-            raf.writeBytes("This will complete the Project.");
-
+            raf.writeBytes("\nThis will complete the Project.");
             for (Deposit dp : deposits) {
-                raf.write(0x0A);
-                raf.writeBytes(dp.getCustomerNumber() + " # " + dp.getPayedInterest());
+                raf.writeBytes("\n" + dp.getCustomerNumber() + " # " + dp.getPayedInterest());
             }
-
             raf.close();
-
         } catch (IOException e) {
             System.out.println("IOException:");
-            e.printStackTrace();
         }
     }
 
