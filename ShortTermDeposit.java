c41a929ca2fb50a47bb28419ae70cb1fed7e8d07
yasirhm <yasaman.rahimee@gmail.com>
2016-10-29 14:03:37 +0330

    ok

--- ShortTermDeposit.java
+++ ShortTermDeposit.java
@@ -1,7 +1,8 @@
 /**
  * Created by Yasi on 10/23/2016.
  */
-public class ShortTermDeposit extends DepositType {
+public class ShortTermDeposit extends DepositType
+{
     public ShortTermDeposit() {
         super(10.0);
     }
