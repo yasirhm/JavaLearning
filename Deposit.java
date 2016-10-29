fb4f5919ae367484dac1d718ba359e9337ce8677
yasirhm <yasaman.rahimee@gmail.com>
2016-10-29 14:04:51 +0330

    ok

--- Deposit.java
+++ Deposit.java
@@ -1,34 +1,28 @@
 /**
- * Created by Dotinschool6 on 10/23/2016.
+ * Created by Yasi on 10/23/2016.
  */
-
 import java.math.BigDecimal;
 import java.math.RoundingMode;
-
-public class Deposit {
-
-    private String customerNumber ;
+public class Deposit
+{
+    private String customerNumber;
     private Integer durationInDays;
     private BigDecimal depositBalance = BigDecimal.ZERO;
-
     private DepositType depositType;
     private BigDecimal payedInterest;
 
-
-    public Deposit(String customerNumber, Integer durationInDays, BigDecimal depositBalance,DepositType depositType) throws Exception {//
+    public Deposit(String customerNumber, Integer durationInDays, BigDecimal depositBalance, DepositType depositType) throws BalanceException, DurationDaysException {
         this.customerNumber = customerNumber;
         this.depositType = depositType;
 
-        if(depositBalance.compareTo(new BigDecimal(0)) == -1){
-            throw new Exception("Wrong: Deposit balance for customer "+customerNumber+" is less than zero!!");
-        }else if (durationInDays <= 0){
-            throw new Exception("Wrong: Duration Days for customer "+customerNumber+" is not valid!!");
-        }
-        else{
+        if (depositBalance.compareTo(new BigDecimal(0)) == -1) {
+            throw new BalanceException("Deposit balance for customer " + customerNumber + " is less than zero!!");
+        } else if (durationInDays <= 0) {
+            throw new DurationDaysException("Duration Days for customer " + customerNumber + " is not valid!!");
+        } else {
             this.depositBalance = depositBalance;
             this.durationInDays = durationInDays;
         }
-
     }
 
     public String getCustomerNumber() {
@@ -51,21 +45,13 @@ public class Deposit {
         this.depositBalance = depositBalance;
     }
 
-    /*public Double getInterestRate() {
-        return interestRate;
-    }
-
-    public void setInterestRate(Double interestRate) {
-        this.interestRate = interestRate;
-    }*/
-
     public BigDecimal getPayedInterest() {
         return payedInterest;
     }
 
-    public void calculatePayedInterest(){
+    public void calculatePayedInterest() {
 
-        BigDecimal Pi = depositBalance.multiply(new BigDecimal(depositType.getInterestRate()).multiply(new BigDecimal(durationInDays) ) ) ;
-        payedInterest =  Pi.divide(new BigDecimal(36500), 3, RoundingMode.CEILING);
+        BigDecimal payedInterests = depositBalance.multiply(new BigDecimal(depositType.getInterestRate()).multiply(new BigDecimal(durationInDays)));
+        payedInterest = payedInterests.divide(new BigDecimal(36500), 3, RoundingMode.CEILING);
     }
 }
